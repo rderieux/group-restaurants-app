86ac83cafc5edaba9ee72be5b400c158a3a464a9
rderieux <ppldiecomplex@gmail.com>
2016-10-21 18:48:03 -0600

    committing to fork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-// import Restaurant from './shared/restaurant';
 import { RestaurantsListComponent } from './restaurants-list/restaurants-list.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import RestaurantService from "./restaurant.service";
@@ -12,14 +12,28 @@ import RestaurantService from "./restaurant.service";
 @NgModule({
   declarations: [
     AppComponent,
-    // Restaurant,
     RestaurantsListComponent,
     RestaurantDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'restaurants',
+        component: RestaurantsListComponent
+      },
+      {
+        path: 'restaurants/:id',
+        component: RestaurantDetailComponent
+      },
+      {
+        path: '',
+        redirectTo: '/restaurants',
+        pathMatch: 'full'
+      }
+    ])
   ],
   providers: [
     RestaurantService
